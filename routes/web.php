8c3d06b6f2128d9a2fa89c59a31c294c9a1307ee
YuDZEN <chenyu.france@gmail.com>
2024-03-21 02:03:22 +0100

    Test database connection with success

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json(['message' => 'Successfully connected to the database.'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['message' => 'Could not connect to the database. Please check your configuration.'], 500);
+    }
+});
+
+Route::get('/env-test', function () {
+return response()->json([
+    'DB_CONNECTION' => env('DB_CONNECTION'),
+    'DB_HOST' => env('DB_HOST'),
+    'DB_PORT' => env('DB_PORT'),
+    'DB_DATABASE' => env('DB_DATABASE'),
+    'DB_USERNAME' => env('DB_USERNAME'),
+    'DB_PASSWORD' => env('DB_PASSWORD'),
+]);
+});
